c8781dc3d9d8aaee4fd26cbd623de3926e54bc20
SaiReddy-A <sai.achakkagari@mcgill.ca>
2021-11-27 02:58:59 -0500

    Changes

--- Snakefile_single.py
+++ Snakefile_single.py
@@ -9,139 +9,93 @@ OUTCOME= ["1","2"]
 
 rule all:
     input:
-        expand("dir_to_each_assembly/assembledGenome/QUAST_OUT_{outcome}_reads_basename/report.txt", outcome=OUTCOME),
-        expand("dir_to_each_assembly/annotatedGenome/{outcome}_reads_basename_full.gb", outcome=OUTCOME)
+        expand("WORKDIR/assembledGenome/QUAST_OUT_{outcome}_prefix/report.txt", outcome=OUTCOME),
+        expand("WORKDIR/annotatedGenome/{outcome}_prefix_full.gb", outcome=OUTCOME)
         
 rule decompress:
     input:
-        compressed_forward="dir_to_raw_reads/forward_read.fastq.gz", 
-        compressed_reverse="dir_to_raw_reads/reverse_read.fastq.gz"
+        compressed_forward="read1", 
+        compressed_reverse="read2"
     output:
-        decompressed_forward="dir_to_raw_reads/forward_read.fastq",
-        decompressed_reverse="dir_to_raw_reads/reverse_read.fastq"
+        decompressed_forward="WORKDIR/reads/forward_read.fastq",
+        decompressed_reverse="WORKDIR/reads/reverse_read.fastq"
     threads:
         threads_available
     shell:
         """
-        echo "$(date): Starts decompressing reads_basename!"
+        echo "$(date): Starts decompressing prefix!"
         """
         """
-        pigz -d -p {threads} -k {input.compressed_forward} {input.compressed_reverse}
+        pigz -d -p {threads} -k -c {input.compressed_forward} > {output.decompressed_forward}
+        pigz -d -p {threads} -k -c {input.compressed_reverse} > {output.decompressed_reverse}
         """
         """
-        echo "$(date): Finished decompressing reads_basename!"
+        echo "$(date): Finished decompressing prefix!"
         """
 
 rule trimmomatic:
     input:
-        read1="dir_to_raw_reads/forward_read.fastq",
-        read2="dir_to_raw_reads/reverse_read.fastq"
+        forward="WORKDIR/reads/forward_read.fastq",
+        reverse="WORKDIR/reads/reverse_read.fastq"
     output:
-        forwardPaired="dir_to_each_assembly/trimmedReads/forward_readP.fastq",
-        reversePaired="dir_to_each_assembly/trimmedReads/reverse_readP.fastq", 
-        forwardUnPaired="dir_to_each_assembly/trimmedReads/forward_readU.fastq", 
-        reverseUnPaired="dir_to_each_assembly/trimmedReads/reverse_readU.fastq"
+        forwardPaired="WORKDIR/trimmedReads/forward_readP.fastq",
+        reversePaired="WORKDIR/trimmedReads/reverse_readP.fastq", 
+        forwardUnPaired="WORKDIR/trimmedReads/forward_readU.fastq", 
+        reverseUnPaired="WORKDIR/trimmedReads/reverse_readU.fastq"
     threads:
         threads_available
     shell:
         """
-        echo "$(date): Trimmomatic for reads_basename is starting!"
+        echo "$(date): Trimmomatic for prefix is starting!"
         """
         """
-        java -jar path_to_trimmomatic PE -threads {threads} {input.read1} {input.read2} {output.forwardPaired} {output.forwardUnPaired} {output.reversePaired} {output.reverseUnPaired} ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:60
+        java -jar path_to_trimmomatic PE -threads {threads} {input.forward} {input.reverse} {output.forwardPaired} {output.forwardUnPaired} {output.reversePaired} {output.reverseUnPaired} ILLUMINACLIP:$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:60
         """
         """
-        echo "$(date): Trimmomatic for reads_basename is finished!"
+        echo "$(date): Trimmomatic for prefix is finished!"
         """
 
 rule novoplasty:
     input:
-        config_novo="dir_to_each_assembly/reads_basename_config.txt",
-        fowardPaired="dir_to_each_assembly/trimmedReads/forward_readP.fastq",
-        reversePaired="dir_to_each_assembly/trimmedReads/reverse_readP.fastq"
+        config_novo="WORKDIR/prefix_config.txt",
+        fowardPaired="WORKDIR/trimmedReads/forward_readP.fastq",
+        reversePaired="WORKDIR/trimmedReads/reverse_readP.fastq"
     output:
-        option1="dir_to_each_assembly/assembledGenome/Option_1_reads_basename.fasta",
-        option2="dir_to_each_assembly/assembledGenome/Option_2_reads_basename.fasta"
+        option1="WORKDIR/assembledGenome/Option_1_prefix.fasta",
+        option2="WORKDIR/assembledGenome/Option_2_prefix.fasta"
     shell:
         """
-        echo "$(date): NOVOPlasty for reads_basename is starting!"
+        echo "$(date): NOVOPlasty for prefix is starting!"
         """
         """
         perl path_to_novoplasty -c {input.config_novo}
         """
         """
-        echo "$(date): NOVOPlasty for reads_basename is finished!"
+        echo "$(date): NOVOPlasty for prefix is finished!"
         """
 
-rule quast:
+rule standardize:
     input:
-        option="dir_to_each_assembly/assembledGenome/Option_{outcome}_reads_basename.fasta"
+        option="WORKDIR/assembledGenome/Option_{outcome}_prefix.fasta"
     output:
-        quastout="dir_to_each_assembly/assembledGenome/QUAST_OUT_{outcome}_reads_basename/report.txt"
-    threads:
-        threads_available
+        standardized="WORKDIR/standardizedGenome/Option_{outcome}_prefix.standardized.fa",
     shell:
         """
-        echo "$(date): QUAST for reads_basename is starting!"
-        """
-        """
-        quast.py -t {threads} -o dir_to_each_assembly/assembledGenome/QUAST_OUT_{wildcards.outcome}_reads_basename {input.option}
-        """
-        """
-        echo "$(date): QUAST for reads_basename is finished!"
-        """
-
-rule nucmer:
-    input:
-        ref="path_to_ref_fasta",
-        option="dir_to_each_assembly/assembledGenome/Option_{outcome}_reads_basename.fasta"
-    output:
-        delta="dir_to_each_assembly/prep/delta_{outcome}_reads_basename.delta",
-        coords="dir_to_each_assembly/prep/coord_{outcome}_reads_basename.coords"
-    threads:
-        threads_available
-    shell:
-        """
-        nucmer -t {threads} --delta={output.delta} {input.ref} {input.option}
-        """
-        """
-        show-coords {output.delta} > {output.coords}
-        """
-
-rule samtools:
-    input:
-        option="dir_to_each_assembly/assembledGenome/Option_{outcome}_reads_basename.fasta",
-        coords="dir_to_each_assembly/prep/coord_{outcome}_reads_basename.coords"
-    output:
-        region1="dir_to_each_assembly/prep/{outcome}_reads_basename_region1.fa",
-        region2="dir_to_each_assembly/prep/{outcome}_reads_basename_region2.fa",
-        full="dir_to_each_assembly/standardizedGenome/{outcome}_reads_basename_full.fa"
-    shell:
-        """
-        samtools faidx {input.option}
-        """
-        """
-        samtools faidx {input.option} Contig1:$(cat {input.coords} |sed '1,5d' |awk '$1==1 {start1insert}')-$(cat {input.coords} |sed '1,5d' |awk '$5>150000 {end1insert}') > {output.region1}
-        """
-        """
-        samtools faidx {input.option} Contig1:0-$(cat {input.coords} |sed '1,5d' |awk '$4<=1 {end2insert}') |sed 1d > {output.region2}
-        """
-        """
-        cat {output.region1} {output.region2} > {output.full}
+        standardize_cpDNA.sh -i {input.option}
         """
 
 rule PGA:
     input:
-        full="dir_to_each_assembly/standardizedGenome/{outcome}_reads_basename_full.fa"
+        full="WORKDIR/standardizedGenome/{outcome}_prefix.standardized.fa"
     output:
-        outfiles="dir_to_each_assembly/annotatedGenome/{outcome}_reads_basename_full.gb"
+        outfiles="WORKDIR/annotatedGenome/{outcome}_prefix_full.gb"
     shell:
         """
-        echo "$(date): PGA for reads_basename is starting!"
+        echo "$(date): PGA for prefix is starting!"
         """
         """
-        perl path_to_PGA -r path_to_ref_gb -t dir_to_each_assembly/standardizedGenome -o dir_to_each_assembly/annotatedGenome
+        perl path_to_PGA -r path_to_ref_gb -t WORKDIR/standardizedGenome -o WORKDIR/annotatedGenome
         """
         """
-        echo "$(date): PGA for reads_basename is finished!"
+        echo "$(date): PGA for prefix is finished!"
         """
--- job_input.sh
+++ job_input.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
 
-dir=/home/norac97/scratch/Glycine #Input here the path to the directory containing raw reads
-current_dir=/home/norac97/scratch/automated_pipeline_norac97 
-config_base=/home/norac97/scratch/automated_pipeline_norac97/config_novo.txt 
-snakefile_base=/home/norac97/scratch/automated_pipeline_norac97/Snakefile_single.py
-seed=/home/norac97/scratch/automated_pipeline_norac97/G_max_seed_atpA.fasta
-ref_fasta=/home/norac97/scratch/automated_pipeline_norac97/Glycine_max.fasta
-ref_gb=/home/norac97/scratch/automated_pipeline_norac97/GBref
-max_memory=40
-threads_available=16
-path_to_trimmomatic=$EBROOTTRIMMOMATIC/trimmomatic-0.39.jar
-path_to_novoplasty=/home/norac97/scratch/NOVOPlasty/NOVOPlasty4.3.1.pl
-path_to_PGA=/home/norac97/scratch/PGA/PGA.pl
+if [ $# -ne 1 ];
+then
+    echo "Usage: 
+        job_input.sh test.cfg"
+    exit 1
+fi
 
-mkdir all_snakefiles
+if [ ! -f $1 ]
+then 
+  echo "config file does not exist"
+else 
+  source $1
 
-for entry in "${dir}"/*
-do
-  mv $entry $(echo "$entry" |sed -r 's|_|-|g; s|-([^-]*)$|\_\1|')
-done
+mkdir ${prefix} && cd ${prefix}
+mkdir reads trimmedReads assembledGenome standardizedGenome annotatedGenome
 
-for entry in "${dir}"/*
-do
-  mkdir $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)
-  cat ${config_base} |sed "s|test|$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)|g" |sed "s|max_memory|${max_memory}|g" |sed "s|path_to_seed|${seed}|g" |sed "s|path_to_reference|${ref_fasta}|g" |sed "s|each_dir|${current_dir}/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)|g" > $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.txt
-done
+WORKDIR=$(pwd)
+read1_novo=${WORKDIR}/trimmedReads/forward_readP.fastq
+read2_novo=${WORKDIR}/trimmedReads/reverse_readP.fastq
 
-for entry in "${dir}"/*
-do
-  if [[ "$(echo "$entry" | rev | cut -d '_' -f 1 | rev)" == *1* ]]; then
-    cat $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.txt |sed "s|read1|$(basename -s .fastq "${entry%.*}")P.fastq|g" > $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.tmp && mv $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.tmp $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.txt
-    cat ${snakefile_base} |sed "s|forward_read|$(basename -s .fastq "${entry%.*}")|g" > all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.py
-  fi
-done
+cat ${config_base} |sed "s|WORKDIR|$WORKDIR|g"| sed "s|test|$prefix|" |sed "s|max_memory|${max_memory}|" | \
+sed "s|path_to_seed|${seed}|" |sed "s|path_to_reference|${ref_fasta}|" |\
+sed "s|read1|$read1_novo|" | sed "s|read2|$read2_novo|" > ${prefix}_config.txt
 
-for entry in "${dir}"/*
-do
-  if [[ "$(echo "$entry" | rev | cut -d '_' -f 1 | rev)" == *2* ]]; then
-    cat $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.txt |sed "s|read2|$(basename -s .fastq "${entry%.*}")P.fastq|g" > $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.tmp && mv $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.tmp $(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_config.txt
-    cat all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.py |sed "s|reverse_read|$(basename -s .fastq "${entry%.*}")|g" > all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.tmp && mv all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.tmp all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.py
-    cat all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.py |sed "s|path_to_ref_fasta|${ref_fasta}|g" |sed "s|path_to_ref_gb|${ref_gb}|g" |sed "s|path_to_trimmomatic|${path_to_trimmomatic}|g" |sed "s|path_to_novoplasty|${path_to_novoplasty}|g" |sed "s|path_to_PGA|${path_to_PGA}|g" |sed "s|dir_to_raw_reads|${dir}|g" |sed "s|dir_to_each_assembly|${current_dir}/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)|g" |sed "s|reads_basename|$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)|g" > all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.tmp && mv all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.tmp all_snakefiles/$(basename -s .fastq "${entry%.*}" | cut -d '_' -f 1)_snakefile.py
-  fi
-done
+if [ $(echo "${read1##*.}") == "gz" ]
+then 
+cat ${snakefile_base} > _tmp.${prefix}_snakefile.py
+else 
+cat ${snakefile_base}|sed "/rule decompress/,/rule trimmomatic/{//p;d;}"|grep -v "rule decompress"| \
+sed "s|WORKDIR/reads/forward_read.fastq|read1|g" |sed "s|WORKDIR/reads/reverse_read.fastq|read2|g" > _tmp.${prefix}_snakefile.py
+fi
 
-for entry in "all_snakefiles"/*
-do
-  cat $current_dir/run.sh |sed "s|each_snakefile|all_snakefiles/$(basename "$entry")|g" > $(basename "${entry%.*}")_job.sh
-  sbatch $(basename "${entry%.*}")_job.sh
-done
+cat _tmp.${prefix}_snakefile.py |sed "s|WORKDIR|$WORKDIR|g"|sed "s|read1|$read1|g" | \
+sed "s|read2|$read2|g" |sed "s|threads_available|$threads_available|" |sed "s|prefix|$prefix|g" |\
+sed "s|path_to_ref_fasta|${ref_fasta}|g" |sed "s|path_to_ref_gb|${ref_gb}|g" |\
+sed "s|path_to_trimmomatic|${path_to_trimmomatic}|g" |sed "s|path_to_novoplasty|${path_to_novoplasty}|g" |\
+sed "s|path_to_PGA|${path_to_PGA}|g"  > ${prefix}_snakefile.py
+
+rm _tmp.${prefix}_snakefile.py
+
+#running snakemake 
+snakemake --unlock
+snakemake -s ${prefix}_snakefile.py -j ${threads_available}
+fi
